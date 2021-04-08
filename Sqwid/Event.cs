9d1e23094153cc7403e33ae78dd0181a53d4d15c
Carly Nield <nieldca@byu.edu>
2021-04-07 19:11:55 -0600

    Revert "Deleted duplicates"
    This reverts commit aa14c7a5dc429f4a0c5558f315509efd3067a991.

---
+++ Sqwid/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Sqwid
+{
+    public partial class Event
+    {
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public int EventId { get; set; }
+        public int EventGroupId { get; set; }
+        public int? EventAdmin { get; set; }
+        public DateTime? EventStartDate { get; set; }
+        public DateTime? EventDueDate { get; set; }
+        public bool? EventPublicVoting { get; set; }
+        public string EventCategory { get; set; }
+
+        public virtual User EventAdminNavigation { get; set; }
+        public virtual Group EventGroup { get; set; }
+    }
+}
