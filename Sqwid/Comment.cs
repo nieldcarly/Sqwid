dcab6b15aca3c461dd5635f0eecebf2595ab4992
Carly Nield <nieldca@byu.edu>
2021-04-07 18:22:08 -0600

    Models and stuff

--- Sqwid/Comment.cs
+++ Sqwid/Comment.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
-namespace Sqwid.Model
+namespace Sqwid
 {
     public partial class Comment
     {
         public string CommentText { get; set; }
-        [Key]
         public int CommentId { get; set; }
         public int CommentCreationId { get; set; }
         public int CommentUserId { get; set; }
---
+++ Sqwid/Favorite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Sqwid
+{
+    public partial class Favorite
+    {
+        public int FavoriteCreationId { get; set; }
+        public int FavoriteUserId { get; set; }
+
+        public virtual Creation FavoriteCreation { get; set; }
+        public virtual User FavoriteUser { get; set; }
+    }
+}
